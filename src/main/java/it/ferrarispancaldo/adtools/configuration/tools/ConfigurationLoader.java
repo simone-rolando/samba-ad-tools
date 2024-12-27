7df8271299728cf7d446db768e3ace725c3fe0e9
simone-rolando <simone.rolando@ferrarispancaldo.net>
2024-12-27 03:51:40 +0100

    Add getters to ConfigurationLoader. Implemented singleton. Add JavaDoc to ConfigurationLoader

--- src/main/java/it/ferrarispancaldo/adtools/configuration/tools/ConfigurationLoader.java
+++ src/main/java/it/ferrarispancaldo/adtools/configuration/tools/ConfigurationLoader.java
@@ -2,7 +2,9 @@ package it.ferrarispancaldo.adtools.configuration.tools;
 
 import it.ferrarispancaldo.adtools.configuration.types.LocalConfiguration;
 import it.ferrarispancaldo.adtools.configuration.types.DomainConfiguration;
+import it.ferrarispancaldo.adtools.logging.LogUtils;
 
+import org.apache.logging.log4j.Logger;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileInputStream;
@@ -10,7 +12,19 @@ import java.io.IOException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * {@code ConfigurationLoader} loads configuration from the system configuration
+ * YAML files.
+ *
+ * <p>It is possible to load configurations from the <strong>default path</strong>
+ * or from a <strong>custom path</strong> passed as parameters.</p>
+ */
 public class ConfigurationLoader {
+    /**
+     * System logger instance
+     */
+    private static final Logger logger = LogUtils.getLogger(ConfigurationLoader.class);
+
     /**
      * Domain configuration default file
      */
@@ -41,14 +55,58 @@ public class ConfigurationLoader {
      */
     private DomainConfiguration domainConfiguration;
 
-    private ConfigurationLoader() {
-
-    }
-
+    /**
+     * Construct a new {@code ConfigurationLoader}
+     * @param localConfigFile path to local configuration file
+     * @param domainConfigFile path to domain configuration file
+     */
     private ConfigurationLoader(String localConfigFile, String domainConfigFile) {
-
+        loadConfig(localConfigFile, domainConfigFile);
     }
 
+    /**
+     * Gets the instance of the {@code ConfigurationLoader}
+     * @return instance of the {@code ConfigurationLoader}
+     */
+    public static ConfigurationLoader getInstance() {
+        return getInstance(LOCAL_CONFIG_FILE, DOMAIN_CONFIG_FILE);
+    }
+
+    /**
+     * Gets the instance of the {@code ConfigurationLoader} from custom config files paths
+     * @param localConfigFile custom local configuration file path
+     * @param domainConfigFile custom domain configuration file path
+     * @return instace of the {@code ConfigurationLoader}
+     */
+    public static ConfigurationLoader getInstance(String localConfigFile, String domainConfigFile) {
+        if (instance == null) {
+            instance = new ConfigurationLoader(localConfigFile, domainConfigFile);
+        }
+
+        return instance;
+    }
+
+    /**
+     * Gets the {@code LocalConfiguration} data from the loader
+     * @return {@code LocalConfiguration} data
+     */
+    public LocalConfiguration getLocalConfiguration() {
+        return localConfiguration;
+    }
+
+    /**
+     * Gets the {@code DomainConfiguration} data from the loader
+     * @return {@code DomainConfiguration} data
+     */
+    public DomainConfiguration getDomainConfiguration() {
+        return domainConfiguration;
+    }
+
+    /**
+     * Loads configuration from the provided config files
+     * @param localConfigFile path to local configuration file
+     * @param domainConfigFile path to domain configuration file
+     */
     private void loadConfig(String localConfigFile, String domainConfigFile) {
         Yaml localConfig = new Yaml();
         Yaml domainConfig = new Yaml();
@@ -56,13 +114,13 @@ public class ConfigurationLoader {
         try (FileInputStream localStream = new FileInputStream(localConfigFile)) {
             localConfiguration = localConfig.loadAs(localStream, LocalConfiguration.class);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.fatal("Error occured while loading local configuration file: {}", localConfigFile, e);
         }
 
         try (FileInputStream domainStream = new FileInputStream(domainConfigFile)) {
             domainConfiguration = domainConfig.loadAs(domainStream, DomainConfiguration.class);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.fatal("Error occured while loading domain configuration file: {}", domainConfigFile, e);
         }
     }
 }
