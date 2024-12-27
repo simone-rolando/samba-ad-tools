4a1b754e7f74018b54a19c63816a59d74ad87d4a
simone-rolando <simone.rolando@ferrarispancaldo.net>
2024-12-27 02:35:38 +0100

    Add skeleton config handling to ConfigurationManager

--- src/main/java/it/ferrarispancaldo/configuration/tools/ConfigurationManager.java
+++ src/main/java/it/ferrarispancaldo/configuration/tools/ConfigurationManager.java
@@ -1,4 +1,69 @@
 package it.ferrarispancaldo.configuration.tools;
 
+import it.ferrarispancaldo.configuration.types.LocalConfiguration;
+import it.ferrarispancaldo.configuration.types.DomainConfiguration;
+
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ConfigurationManager {
+    /**
+     * Domain configuration default file
+     */
+    public static final String DOMAIN_CONFIG_FILE = "/etc/adtools/domain.yaml";
+
+    /**
+     * Local configuration default file
+     */
+    public static final String LOCAL_CONFIG_FILE = "/etc/adtools/local.yaml";
+
+    /**
+     * Singleton static instance of this class
+     */
+    private static ConfigurationManager instance;
+
+    /**
+     * Global concurrency lock
+     */
+    private static final Lock lock = new ReentrantLock();
+
+    /**
+     * Local configuration data
+     */
+    private LocalConfiguration localConfiguration;
+
+    /**
+     * Domain configuration data
+     */
+    private DomainConfiguration domainConfiguration;
+
+    private ConfigurationManager() {
+
+    }
+
+    private ConfigurationManager(String localConfigFile, String domainConfigFile) {
+
+    }
+
+    private void loadConfig(String localConfigFile, String domainConfigFile) {
+        Yaml localConfig = new Yaml();
+        Yaml domainConfig = new Yaml();
+
+        try (FileInputStream localStream = new FileInputStream(localConfigFile)) {
+            localConfiguration = localConfig.loadAs(localStream, LocalConfiguration.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileInputStream domainStream = new FileInputStream(domainConfigFile)) {
+            domainConfiguration = domainConfig.loadAs(domainStream, DomainConfiguration.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
