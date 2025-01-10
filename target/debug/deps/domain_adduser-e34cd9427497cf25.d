c3fde116808a183f163fe111c2ed464b439dc258
simone-rolando <simone.rolando@ferrarispancaldo.net>
2025-01-10 11:32:49 +0100

    Add resolver to workspace

---
+++ target/debug/deps/domain_adduser-e34cd9427497cf25.d
@@ -0,0 +1,5 @@
+/Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/target/debug/deps/domain_adduser-e34cd9427497cf25: domain-adduser/src/main.rs
+
+/Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/target/debug/deps/domain_adduser-e34cd9427497cf25.d: domain-adduser/src/main.rs
+
+domain-adduser/src/main.rs:
---
+++ target/debug/domain-adduser.d
@@ -0,0 +1,1 @@
+/Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/target/debug/domain-adduser: /Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/domain-adduser/src/main.rs
