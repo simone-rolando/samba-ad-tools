e23d06c1561b4ad6c4df376bb58462be93d7e3ce
simone-rolando <simone.rolando@ferrarispancaldo.net>
2025-01-10 11:34:45 +0100

    Add .gitignore and workspace settings

--- target/debug/deps/domain_adduser-e34cd9427497cf25.d
+++
@@ -1,5 +0,0 @@
-/Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/target/debug/deps/domain_adduser-e34cd9427497cf25: domain-adduser/src/main.rs
-
-/Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/target/debug/deps/domain_adduser-e34cd9427497cf25.d: domain-adduser/src/main.rs
-
-domain-adduser/src/main.rs:
--- target/debug/domain-adduser.d
+++
@@ -1,1 +0,0 @@
-/Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/target/debug/domain-adduser: /Users/m4ce/Documents/Scuola/Tools/samba-ad-tools/domain-adduser/src/main.rs
